3ddc1af1fbcb32c1e6adafa54af8c394ddfe84ff
naimulnobel <nobelnaimulislam@gmail.com>
2023-02-13 19:37:43 +0600

    array

---
+++ data stracture/array.js
@@ -0,0 +1,12 @@
+const strings=['a','b','c','d']
+//4*4
+//push(0(1))
+strings.push('e')
+//pop(0(1))
+strings.pop()
+strings.pop()
+//unshift(0(n))
+strings.unshift('f')
+//splice(0(N))
+strings.splice(2,0,'alien')
+console.log(strings)
